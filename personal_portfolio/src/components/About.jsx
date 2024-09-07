46dbc7a7b7ac68b45bcb748f4c9a0e5cd8a18019
Dhruv Chokshi <168044980+chokshidhruv@users.noreply.github.com>
2024-09-07 18:33:29 -0400

    Update About.jsx

--- personal_portfolio/src/components/About.jsx
+++ personal_portfolio/src/components/About.jsx
@@ -74,7 +74,7 @@ const About = () => {
                             })
                         }   
                         <div className='flex lg:hidden justify-center text-center gap-4 mt-8'>
-                            <motion.a variants={iconVariant(1/3)} initial={'hiddenOpp'} animate={'visible'} whileHover={{scale: 1.1}} whileTap={{scale: 0.9}} target='_blank' href='https://drive.google.com/file/d/1QhCrPFrKLfIriI6EJpft3S5cFrF_7PGG/view?usp=sharing' className="bg-blue-400 text-slate-950 font-bold cursor-pointer uppercase tracking-wider flex items-center px-6 py-1 rounded-full text-md z-40">Resume</motion.a>
+                            <motion.a variants={iconVariant(1/3)} initial={'hiddenOpp'} animate={'visible'} whileHover={{scale: 1.1}} whileTap={{scale: 0.9}} target='_blank' href='https://drive.google.com/file/d/1hfjkLW-L6jjDdPppEKyzT36IhdDLYvBW/view?usp=sharing' className="bg-blue-400 text-slate-950 font-bold cursor-pointer uppercase tracking-wider flex items-center px-6 py-1 rounded-full text-md z-40">Resume</motion.a>
                             <motion.a variants={iconVariant(1/3)} initial={'hiddenOpp'} animate={'visible'} whileHover={{scale: 1.1}} whileTap={{scale: 0.9}} target='_blank' href='https://www.notion.so/Dhruv-Chokshi-85e16a82406f40eaa48235eaf8129a6c' className="bg-slate-950 border-4 border-blue-400 text-blue-400 cursor-pointer font-bold uppercase tracking-wider flex items-center text-mf px-6 py-1 rounded-full z-40">Portfolio</motion.a>
                         </div>
                     </div>
@@ -87,11 +87,11 @@ const About = () => {
                         </div>
                         <div className='w-full text-center grid items-start justify-center py-8 mt-2 border opacity-75 rounded-xl border-slate-900'>
                             <p className='text-xl font-semibold text-blue-400'>Current cGPA</p>
-                            <p className='text-lg font-normal pt-3 text-slate-400'>10.4/12 (3.80/4.00)</p>
+                            <p className='text-lg font-normal px-3 pt-3 text-slate-400'>10.4/12 (3.80/4.00)</p>
                         </div>
                         <div className='w-full text-center grid items-start justify-center py-8 mt-2 border opacity-75 rounded-xl border-slate-900'>
                             <p className='text-xl font-semibold text-blue-400'>Relevant Coursework</p>
-                            <p className='text-lg font-normal pt-3 text-slate-400'>View All My Completed Courses (Available Soon!)</p>
+                            <p className='text-lg font-normal px-3 pt-3 text-slate-400'>View All My Completed Courses (Available Soon!)</p>
                             <a className='text-xl items-center flex justify-center py-3 hover:text-blue-500 cursor-pointer' href='#' ><AiOutlineExport /></a>
                         </div>
 
